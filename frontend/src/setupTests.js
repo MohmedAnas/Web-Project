97cd824da3a526f2178cc37886c3820d00869176
MohmedAnas <143054234+MohmedAnas@users.noreply.github.com>
2025-09-09 10:27:35 +0530

    Update setupTests.js

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,25 +1,19 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
 // Mock IntersectionObserver
 global.IntersectionObserver = class IntersectionObserver {
   constructor() {}
-  disconnect() {}
-  observe() {}
-  unobserve() {}
+  disconnect() { /* no-op */ }
+  observe() { /* no-op */ }
+  unobserve() { /* no-op */ }
 };
-
 // Mock ResizeObserver
 global.ResizeObserver = class ResizeObserver {
   constructor() {}
-  disconnect() {}
-  observe() {}
-  unobserve() {}
+  disconnect() { /* no-op */ }
+  observe() { /* no-op */ }
+  unobserve() { /* no-op */ }
 };
-
 // Mock window.matchMedia
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
@@ -34,7 +28,6 @@ Object.defineProperty(window, 'matchMedia', {
     dispatchEvent: jest.fn(),
   })),
 });
-
 // Mock localStorage
 const localStorageMock = {
   getItem: jest.fn(),
